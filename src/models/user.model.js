a2c97dc787ccefa546a666539dcd1a479850de15
Bipin <beepeenstha10@gmail.com>
2025-12-10 10:35:53 +0545

    add user and video model

---
+++ src/models/user.model.js
@@ -0,0 +1,106 @@
+import mongoose, { schema } from "mongoose";
+import jwt from "jsonwebtoken";
+import bcrypt from "bcrypt";
+
+const userSchema = new Schema(
+  {
+    username: {
+      type: String,
+      required: true,
+      unique: true,
+      lowercase: true,
+      trim: true,
+      index: true,
+    },
+    email: {
+      type: String,
+      required: true,
+      unique: true,
+      lowercase: true,
+      trim: true,
+    },
+    fullName: {
+      type: String,
+      required: true,
+      trim: true,
+      index: true,
+    },
+    avatar: {
+      type: String, //cl;oudnary url
+      required: true,
+    },
+    coverImage: {
+      type: String, //cloudnary url
+    },
+    watchHistory: [
+      {
+        type: Schema.Types.ObjectId,
+        ref: "Video",
+      },
+    ],
+    password: {
+      type: String,
+      required: [true, "Password is required"],
+    },
+    refreshToken: {
+      type: String,
+    },
+  },
+  {
+    timestamps: true,
+  }
+);
+
+// here we are not using ()=>{} an arrow function
+// because inside arrow function there is not the reference of "this"
+// meaning the arrow function doesn't know the context
+// here in this block, it is very much important to know the context
+// as the following save event is working on the user
+// so instead we write function () {}
+// since it is a middleware, next is used to indicate to pass the flag after it's completion
+// process like encryption takes time so it is good to use async-await
+userSchema.pre("save", async function (next) {
+  // this logic has been implemented to avoid the encryption in case of no change in password
+  if (!this.isModified("password")) {
+    return next();
+  }
+
+  this.password = await bcrypt.hash(this.password, 10);
+  next();
+});
+
+// mongoose has its own methods
+// mongoose also allows us to make out custom method
+// for that we use schema and under which an object called methods is provided to add the method
+userSchema.methods.isPasswordCorrect = async function (password) {
+  return await bcrypt.compare(password, this.password);
+};
+
+userSchema.methods.generateAccessToken = function () {
+  return jwt.sign(
+    {
+      _id: this._id,
+      email: this.email,
+      username: this.username,
+      fullName: this.fullName,
+    },
+    process.env.ACCESS_TOKEN_SECRET,
+    {
+      expiresIn: process.env.ACCESS_TOKEN_EXPIRY,
+    }
+  );
+};
+
+userSchema.methods.generateRefreshToken = function () {
+  return jwt.sign(
+    {
+      _id: this._id,
+    },
+    process.env.REFRESH_TOKEN_SECRET,
+    {
+      expiresIn: process.env.REFRESF_TOKEN_EXPIRY,
+    }
+  );
+};
+
+export const User = mongoose.model("User", userSchema);
